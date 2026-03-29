88c07fdcd47bb976ea6599093580361e70805856
Amir Rahi <amirrahi29@gmail.com>
2026-03-29 15:34:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send("Server chal raha hai 🚀");
+});
+
+app.get('/api', (req, res) => {
+    res.json({
+        message: "API working on api endpoint",
+        status: true
+    });
+});
+
+app.get('/api/data', (req, res) => {
+    const data = req.body;
+
+    res.json({
+        message: "Data mil gaya 👍",
+        receivedData: data
+    });
+});
+
+// 404 Handler
+app.use((req, res) => {
+    res.status(404).json({ message: "Route nahi mila ❌" });
+});
+
+// Server Start
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
